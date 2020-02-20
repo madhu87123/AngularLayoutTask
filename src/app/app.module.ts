4f598a48103a9c8759fff42c88cc7bcd15e39cfc
madhupriya prabhakararao <madhupriya.prabhakararao@ameexusa.com>
2020-02-20 18:47:23 +0530

    second task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    BoxesComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
